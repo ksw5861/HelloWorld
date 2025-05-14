ac5bab59bd43eba66beb86b38d16723e53f27978
Sangwoo, Kim <kimsw5861@naver.com>
2025-05-14 10:47:01 +0900

    첫번째 릴리즈

---
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.yedam;
+// 자바: 컴파일(***.java -> ***.class 파일로 생성된다)
+public class HelloWorld {
+	// main 메소드.
+	public static void main(String[] args) {
+		for (int i = 1; i <=3; i++) {
+			System.out.println("hello, world");
+		}
+	}
+}
+
