7490d5f2e85c5948b66d9176c4f3b1d7ca4b69bd
Sangwoo, Kim <kimsw5861@naver.com>
2025-05-14 12:42:18 +0900

    충돌?

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,13 +2,17 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
+		test2();
+	} // end of main.
+
+	public static void test2() {
 		// 임의의 수를 생성. 1 ~ 100사이의 임의의 값 생성.
-		//1 <= x < 11
+		// 1 <= x < 11
 		int sum = 0;
-		for(int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= 5; i++) {
 			int result = (int) (Math.random() * 100) + 1; // 0 <= x < 1 실수 생성
 			sum += result;
-			System.out.println(result);
+			System.out.println(result); // 생성된 난수 출력
 		}
 		System.out.println("결과는 " + sum + "입니다.");
 	} // end of main.
